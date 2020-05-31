71a6723b15e9b12fd0d815d3cb5cd32099bd325c
nhowlade <nhowlade@uoguelph.ca>
2020-05-31 18:11:00 -0400

    add queryselectors

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+    const width = 10;
+    const grid = document.querySelector('.grid');
+    let squares  = Array.from(document.querySelectorAll('.grid div'));
+    const ScoreDisplay = document.querySelector('#score');
+    const StartBtn = document.querySelector('#start-button');
 })
