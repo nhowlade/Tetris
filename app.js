15ee373e8d817e3aa01a310ca6eb2472134211d2
nhowlade <nhowlade@uoguelph.ca>
2020-05-31 23:02:18 -0400

    Two more tables to go

--- app.js
+++ app.js
@@ -4,4 +4,26 @@ document.addEventListener('DOMContentLoaded', () => {
     let squares  = Array.from(document.querySelectorAll('.grid div'));
     const ScoreDisplay = document.querySelector('#score');
     const StartBtn = document.querySelector('#start-button');
+
+    //The Tetreminos
+    const lTetremino = [
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
+        [width, width*2, width*2+1, width*2+2]
+    ]
+
+    const zTetremino = [
+        [width*2, width+1, width*2+1, width+2],
+        [0, width, width+1, width*2+1],
+        [width*2, width+1, width*2+1, width+2],
+        [0, width, width+1, width*2+1]
+    ]
+
+    const tTetremino = [
+        [width, 1, width+1, width+2],
+        [1, width+1, width*2+1, width+2],
+        [width, width+1, width*2+1, width+2],
+        [width, 1, width+1, width*2+1]
+    ]
 })
