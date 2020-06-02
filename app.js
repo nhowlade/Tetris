7b60699d35556d174429fd2cd9990891af9f3052
nhowlade <nhowlade@uoguelph.ca>
2020-06-01 22:15:29 -0400

    Moving Tetris

--- app.js
+++ app.js
@@ -26,4 +26,64 @@ document.addEventListener('DOMContentLoaded', () => {
         [width, width+1, width*2+1, width+2],
         [width, 1, width+1, width*2+1]
     ]
+
+    const oTetrimino =[
+        [0, width, 1, width+1],
+        [0, width, 1, width+1],
+        [0, width, 1, width+1],
+        [0, width, 1, width+1]
+    ]
+
+    const iTetremino = [
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3],
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3]
+    ]
+
+    const theTetriminoes = [lTetremino, zTetremino, tTetremino, oTetrimino, iTetremino];
+    //randomly select a tetrimono
+    let random = Math.floor(Math.random()*theTetriminoes.length);
+    let currentPosition = 4;
+    let currentRotation = 0;
+    let current = theTetriminoes[random][currentRotation];
+
+    //draew the first rotation of a tetrimino
+
+    function draw()
+    {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.add('tetrimino');
+        })
+    }
+
+    function undraw() {
+        current.forEach(index => {
+            squares[currentPosition+index].classList.remove('tetrimino');
+        })
+    }
+
+
+    //make the tetrimino to go
+    timerId = setInterval(moveDown, 900);
+
+    function moveDown()
+    {
+        undraw();
+        currentPosition += width;
+        draw();
+    }
+
+    function free()
+    {
+        if(current.some(index => squares[currentPosition + index + width].classList.contains('taken')))
+        {
+            current.forEach( index => squares[currentPosition + index].classList.add('taken'));
+
+            random = Math.floor(Math.random()* theTetriminoes.length);
+            current = theTetriminoes[random][currentRotation];
+            currentPosition = 4;
+            
+        }
+    }
 })
